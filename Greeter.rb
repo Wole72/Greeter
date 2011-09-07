1d477d34014415b48f885a883df24895563e4b72
Oluwawole <oluwawole@oluwawole-VirtualBox.(none)>
2011-09-07 14:24:37 -0400

    Created new version of another placeholder for Greeter class

--- Greeter.rb
+++ Greeter.rb
@@ -1,14 +1,49 @@
 #!/usr/bin/env ruby
 # This script will greet people and say hello
 
-class Greeter 
-  def hello name
-      puts "Hello, #{name}!"
+module GreetingAandFarewell
+ def name
+
+ def name
+    @name || = "person who will give me money for some product service"
+ end
+
+ def name = new_name
+     @name = new_name
+ end
+
+
+  def hello 
+      puts "Hello, #{@name}!"
       #Greeter.hello "Oluwawole"
+  end
+  	
+  def farewell 
+  puts " Farewell, #{@name}!"
+end
+
+class Greeter 
+  include GreetingAandFarewell
+ 
+ def initialize new_name = nil
+     @name = new_name
+  end
+end
+
+class Customer
+  include GreetingAandFarewell
+
+ def name 
+    @name || =  "guy who will provide me with a good or service in exchange for monetory compensation"
   end
 end
 
 
-my_greeter = Greeter.new
-my_greeter.hello ARGV.first
+#my_greeter = Greeter.new AGRV.first
+#name = ARGV.first
+#my_greeter.hello 
+#my_greeter.farewell
+
+# call sercurity
+# farewell
 
