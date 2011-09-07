8c4a129c64572d0b31c7146ac710c4db1af69cb1
Oluwawole <oluwawole@oluwawole-VirtualBox.(none)>
2011-09-07 13:22:55 -0400

    Created a another placeholder for Greeter class

--- Greeter.rb
+++ Greeter.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-# This script will greet people and say hello 
+# This script will greet people and say hello
+
+class Greeter 
+  def hello name
+      puts "Hello, #{name}!"
+      #Greeter.hello "Oluwawole"
+  end
+end
+
+
+my_greeter = Greeter.new
+my_greeter.hello ARGV.first
+
